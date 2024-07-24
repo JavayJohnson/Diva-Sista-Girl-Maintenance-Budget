3ead04613d31caabc2a121c6537e5e65bbe20d56
javay johnson <javayjohnson@gmail.com>
2024-07-24 10:57:58 -0400

    second Push

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// let anExample = "this"
-// let calculatedExample = 723 + 44 
-// let templateLit = `Both ${anExample} but also ${calculatedExample} as a String`
-// // ---------------------------variableDeclarations
-
-
-
-// const demo = () => {
-//   let userInput = prompt(" Give me a number to add");
-//   //   Get Users Input
-//   if (userInput === "10") {
-//     alert("Perfect 10 BRO");
-//   } else {
-//     alert("Thats Just the way it goes.");
-//   }
-//   console.log(userInput);
-// };
-// // Demo 1 _ IF Statement
 
 // const legalAgeFunc = () => {
 //   let usersAge = prompt(" Enter Age");
@@ -32,70 +14,71 @@
 // // Demo 2 _ IF Statement MULTI Conditional
 
 
-// // Conditional Breakdown: 
-//     // if(userInput === '10'){
-//     //     alert("Perfect 10 BRO")
-//     // }
-//     // else{
-//     //     alert("Thats Just the way it goes.")
-//     // }
+// Conditional Breakdown: 
+    // if(userInput === '10'){
+    //     alert("Perfect 10 BRO")
+    // }
+    // else{
+    //     alert("Thats Just the way it goes.")
+    // }
 
-// // =================if Statement: uses () to evaluate a stmnt to see if result = true.
-//     // --> true : {}first code block executes
-//     // --> false: {}else code block executes
+// =================if Statement: uses () to evaluate a stmnt to see if result = true.
+    // --> true : {}first code block executes
+    // --> false: {}else code block executes
 
 
-//-----------------------------------------------------------------------------------------------------------------
-  //create 10 variables
-//Diva Well-being Monthly Maintenance Budget
-  let monthlyBudget = 1700;
-  let hairSalon = 190;
-  let nailSpa = 110;
-  let skinCare = 100;
+// -----------------------------------------------------------------------------------------------------------------
+//   create 10 variables
+// Diva Well-being Monthly Maintenance Budget
+  let monthlyBudget = 2000;
+  let hairSalon = 200;
+  let nailSpa = 150;
+  let skinCareproducts = 100;
   let fitnessClass = 250;
   let wardrobeSubscriptions = 300;
   let facial = 150;
   let massage = 100;
   let yoga = 100;
   let foodSubscription = 250;
-  let sistaGirlOutings = 300;
-  let totalExpenses = hairSalon + nailSpa + skinCare + fitnessClass + wardrobeSubscriptions + facial + massage + yoga + foodSubscription + sistaGirlOutings;
+  let poppingChampagne = 300;
+  let totalExpenses = hairSalon + nailSpa + skinCareproducts + fitnessClass + wardrobeSubscriptions + facial + massage + yoga + foodSubscription + poppingChampagne;
 
   // 2. Perform calculations(*operator) on vars and create new Vars
 //caculate remaining balance
-let remainingBalance  = totalExpenses - monthlyBudget;
-let OverYourBudget = monthlyBudget - totalExpenses;
+// let remainingBalance  = totalExpenses - monthlyBudget;
+let remainingBalance = monthlyBudget - totalExpenses;
 
 //3. Create 3 if Statements and test their outputs in console.
-  if (remainingBalance > 0) {
-    console.log("You have $" + remainingBalance + " left! Keep slaying, Javay!");
-  } 
-  
-  else if (remainingBalance === 0) {
-    console.log("you have successfully balanced your Diva Well-being Monthly Maintenance Budget! Stay beautiful Girlie");
-  }
 
-else if (remainingBalance < 0) {
-  console.log("So, you need to cut back, or either get a part-time job. Just keeping it real sis");
+if (remainingBalance > 0) {
+  console.log(`${remainingBalance} is positive. You have $${remainingBalance} left! Keep slaying, Javay! You are stacking RACKS on RACKS on RACKS! 💵💵💵`);
 }
+else if (remainingBalance === 0) {
+  console.log(`${remainingBalance} is zero. Neutral. That's What's up Javay! You have successfully balanced your ${monthlyBudget} Diva Well-being Monthly Maintenance Budget to ${remainingBalance}! Stay beautiful Girlie!`);
+}
+else if (remainingBalance < 0) {
+  console.log(`${remainingBalance} is negative. So, you need to cut back, or either get a part-time job fr fr. You have gone over your ${monthlyBudget}! I'm just keeping it 100 Sis, you are NOT managing your money well! Chill on 🍾🥂 popping bottles 🍾🥂 for a while.`);}
 
 // 4. Use 2 Logical Operators
-let onBudget = 2;
-let overBudget = 0; 
+let onBudget = 0;
+let overBudget = 2; 
 let underBudget = 1;
 let goodFinances = true
-let badFinances = true
+let badFinances = false
 let ontarget = true
 
 
-if (ontarget = onBudget >= 2) {
-console.log("You are managing your money well!")
-}
+// if (ontarget & onBudget ===0) {
+// console.log("You are an accounting pro 📈, Sis!")
+// }
 
-if (badFinances = overBudget = 0) {
-console.log("You are NOT managing your money well!")
-}
+// if (badFinances || overBudget >= 2) {
+// console.log("Good luck on becoming that millionaire!")
+// }
 
+// if (goodFinances & underBudget <= 1) {
+//   console.log("💵💵💵 Welcome to the Millionaire Women's Club 💵💵💵")
+// }
 
       // 5. Interpolate All 10 of your variables in a console log
 
@@ -103,23 +86,23 @@ console.log(`Diva Sista Girl Maintenance Budget
     monthlyBudget: ${monthlyBudget}
     hairSalon: ${hairSalon}
     nailSpa: ${nailSpa}
-    skinCare: ${skinCare}
+    skinCareproducts: ${skinCareproducts}
     fitnessClass: ${fitnessClass}
     wardrobeSubscriptions: ${wardrobeSubscriptions} 
     facial: ${facial}
     massage: ${massage}
     yoga: ${yoga}
     foodSubscription: ${foodSubscription} 
-    sistaGirlOutings: ${sistaGirlOutings}
+    poppingChampagne: ${poppingChampagne}
     remainingBalance: ${remainingBalance}`);
 
 
-  //calculate total expense
-let remainingBalance2 = (monthlyBudget - totalExpenses);
-let OverYourBudget2 = (totalExpenses - monthlyBudget);
+   //caculate remaining budget
+let balance = (monthlyBudget - totalExpenses);
+// let Remainingbalance = (totalExpenses - monthlyBudget);
 
 
-  //caculate remaining budget
+ 
 
   
 
